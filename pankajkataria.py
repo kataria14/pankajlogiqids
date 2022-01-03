e79a48dd40a30e7b1e84670e268cea6e2801aff0
kataria14 <97040379+kataria14@users.noreply.github.com>
2022-01-03 21:07:13 +0530

    Add files via upload

---
+++ pankajkataria.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+s=['superman','thor','robin','ironman','ghostrider','captainamerica','flash','wolverine','batman','hulk','blade','phantom','spiderman','blackwidow','hellboy','punisher']
+n=[78737626,8467,76246,4766626,4467874337,22782462637422,35274,965837463,228626,4855,25233,7426866,774337626,2522594369,4355269,78647437]
+while(input):
+    
+    
+    
+    c=int(input())
+    for i in range(len(n)):
+        if(c==n[i]):
+            print(s[i])
+            break
+
+
+# In[ ]:
+
+
+
+
